4c30eb925c87beb92e2a7d20e5d2a4d7a635ca60
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-12-22 21:36:39 +0200

    Update schema

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+
+ActiveRecord::Schema[7.0].define(version: 2022_12_22_184048) do
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
