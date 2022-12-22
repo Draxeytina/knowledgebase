d4fbbdc35d7e9da2f70b86fdd61396b5c14f3a2e
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-12-22 21:32:59 +0200

    Add articles controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show edit update destroy ]
+
+  # GET /articles or /articles.json
+  def index
+    @articles = Article.all
+  end
+
+  # GET /articles/1 or /articles/1.json
+  def show
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+end
